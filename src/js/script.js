d8af8f7bab2d5c30df70062bcd2ac623ba3e3162
Polina <991201@bk.ru>
2021-05-20 11:19:43 +0300

    create validate forms

--- src/js/script.js
+++ src/js/script.js
@@ -68,4 +68,29 @@ $(document).ready(function(){
           $('.overlay, #order').fadeIn('slow');
         })
       });
+
+      function validateForms(form) {
+        $(form).validate({
+          rules: {
+            name: "required",
+            phone: "required",
+            email: {
+              required: true,
+              email: true
+            }
+          },
+          messages: {
+            name: "Пожалуйста, введите своё имя",
+            phone: "Пожалуйста, введите свой номер телефона",
+            email: {
+              required: "Пожалуйста, введите свою почту",
+              email: "Неправильно введен адрес почты"
+            }
+          }
+        });
+      };
+
+      validateForms('#consultation-form');
+      validateForms('#order form');
+      validateForms('#consultation form');
   });
